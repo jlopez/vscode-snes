fc0b5ee258c3904faaa19299dfaa05ab2fe6b554
Jesus Lopez <jesus@jesusla.com>
2024-07-10 20:17:26 -0700

    wip: native addon class

--- asar/src/asar.cc
+++ asar/src/asar.cc
@@ -1,28 +1,80 @@
 #include <napi.h>
 
-// ...
+class Example : public Napi::ObjectWrap<Example> {
+  public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    Example(const Napi::CallbackInfo& info);
+    static Napi::Value CreateNewItem(const Napi::CallbackInfo& info);
 
-/**
-* This code is our entry-point. We receive two arguments here, the first is the
-* environment that represent an independent instance of the JavaScript runtime,
-* the second is exports, the same as module.exports in a .js file.
-* You can either add properties to the exports object passed in or create your
-* own exports object. In either case you must return the object to be used as
-* the exports for the module when you return from the Init function.
-*/
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  private:
+    double _value;
+    Napi::Value GetValue(const Napi::CallbackInfo& info);
+    Napi::Value SetValue(const Napi::CallbackInfo& info);
+};
 
-  // ...
+Napi::Object Example::Init(Napi::Env env, Napi::Object exports) {
+    // This method is used to hook the accessor and method callbacks
+    Napi::Function func = DefineClass(env, "Example", {
+        InstanceMethod<&Example::GetValue>("GetValue", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
+        InstanceMethod<&Example::SetValue>("SetValue", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
+        StaticMethod<&Example::CreateNewItem>("CreateNewItem", static_cast<napi_property_attributes>(napi_writable | napi_configurable)),
+    });
 
-  return exports;
+    Napi::FunctionReference* constructor = new Napi::FunctionReference();
+
+    // Create a persistent reference to the class constructor. This will allow
+    // a function called on a class prototype and a function
+    // called on instance of a class to be distinguished from each other.
+    *constructor = Napi::Persistent(func);
+    exports.Set("Example", func);
+
+    // Store the constructor as the add-on instance data. This will allow this
+    // add-on to support multiple instances of itself running on multiple worker
+    // threads, as well as multiple instances of itself running in different
+    // contexts on the same thread.
+    //
+    // By default, the value set on the environment here will be destroyed when
+    // the add-on is unloaded using the `delete` operator, but it is also
+    // possible to supply a custom deleter.
+    env.SetInstanceData<Napi::FunctionReference>(constructor);
+
+    return exports;
 }
 
-/**
-* This code defines the entry-point for the Node addon, it tells Node where to go
-* once the library has been loaded into active memory. The first argument must
-* match the "target" in our *binding.gyp*. Using NODE_GYP_MODULE_NAME ensures
-* that the argument will be correct, as long as the module is built with
-* node-gyp (which is the usual way of building modules). The second argument
-* points to the function to invoke. The function must not be namespaced.
-*/
+Example::Example(const Napi::CallbackInfo& info) :
+    Napi::ObjectWrap<Example>(info) {
+  // ...
+  Napi::Number value = info[0].As<Napi::Number>();
+  this->_value = value.DoubleValue();
+}
+
+Napi::Value Example::GetValue(const Napi::CallbackInfo& info){
+    Napi::Env env = info.Env();
+    return Napi::Number::New(env, this->_value);
+}
+
+Napi::Value Example::SetValue(const Napi::CallbackInfo& info){
+    // ...
+    Napi::Number value = info[0].As<Napi::Number>();
+    this->_value = value.DoubleValue();
+    return this->GetValue(info);
+}
+
+// Initialize native add-on
+Napi::Object Init (Napi::Env env, Napi::Object exports) {
+    Example::Init(env, exports);
+    return exports;
+}
+
+// Create a new item using the constructor stored during Init.
+Napi::Value Example::CreateNewItem(const Napi::CallbackInfo& info) {
+  // Retrieve the instance data we stored during `Init()`. We only stored the
+  // constructor there, so we retrieve it here to create a new instance of the
+  // JS class the constructor represents.
+  Napi::FunctionReference* constructor =
+      info.Env().GetInstanceData<Napi::FunctionReference>();
+  return constructor->New({ Napi::Number::New(info.Env(), 42) });
+}
+
+// Register and initialize native add-on
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
--- asar/src/index.ts
+++ asar/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello");
+const { Example } = require('bindings')('asar');
+
+const example = new Example(11);
+console.log(example.GetValue());
+// It prints 11
+example.SetValue(19);
+console.log(example.GetValue());
+// It prints 19
